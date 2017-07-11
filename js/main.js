916391cb010e93e6a86850b020341fcfc9225279
sarah lund <sarah.christine.lund@gmail.com>
2017-07-11 17:03:55 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo= "king";
-var cardThree= "";
-var cardFour= "";
-
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push("cardOne");
+var cardTwo = cards[2];
+cardsInPlay.push ("cardTwo");
 console.log("User flipped" + " " + cardOne);
 console.log("User flipped" + " " + cardTwo);
+if (cardsInPlay.length === 2) {
+} else if (cardsInPlay[0] === cardsInPlay[2])
+alert("You found a match!");{
+} if (cardsInPlay[0] !== cardsInPlay[1])
+alert("Sorry try again!");
